6d09e93fb9a97310bab706f9fc04bb0bcfa9de7f
Thomas Vautrin <tvautrin.work@gmail.com>
2016-10-29 18:20:41 +0200

    Mise en place ng2 admin

---
+++ web/src/main/front/src/app/pages/forms/components/inputs/components/selectInputs/selectInputs.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'select-inputs',
+  styles: [require('./selectInput.scss')],
+  template: require('./selectInputs.html')
+})
+export class SelectInputs {
+  constructor() {
+  }
+}
