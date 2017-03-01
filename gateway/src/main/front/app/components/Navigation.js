b6965428b52abfcdcb4785efdc65d7c1438b555c
Thomas Vautrin <thomas-externe.vautrin@edf.fr>
2017-03-01 17:40:54 +0100

    Sécurité + Layout

--- gateway/src/main/front/app/components/Navigation.js
+++ gateway/src/main/front/app/components/Navigation.js
@@ -1,17 +1,52 @@
 import React, {Component} from "react";
 import {Link} from "react-router";
-import LoginButton from './LoginButton';
+import UserContext from "../data/UserContext";
+
+var client = require('../api/client');
 
 export default class Navigation extends Component {
 
+
+    constructor(props, context) {
+        super(props, context);
+        var userCtx = new UserContext('anonymous');
+        this.state = {isAuthenticated : false, user : userCtx}
+    };
+
+    componentWillMount(){
+        client({
+            method: 'GET',
+            path: '/api/user',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }).then(response => {
+            if(response.entity !== ""){
+                this.setState({isAuthenticated: true, user : response.entity});
+            }
+        }, response => {
+            if (response.status.code === 403) {
+                alert('ACCESS DENIED: You are not authorized to update ');
+            }
+            if (response.status.code === 412) {
+                alert('DENIED: Unable to update ');
+            }
+            if (response.status.code === 302) {
+                alert('DENIED: Redirection ');
+                window.location.href = response.headers.location;            }
+        });
+    }
+
     render() {
 
+        var userCtx = new UserContext(this.state.user.login, this.state.user.authorities);
         return (
             <div>
                 <Link to='/'>Home</Link>&nbsp;
                 <Link to='/search'>Recherche</Link>&nbsp;&nbsp;
-                <Link to='/admin'>Administration</Link>&nbsp;&nbsp;
-                <LoginButton />
+                { userCtx.hasAuthority("ROLE_ADMIN") ? <Link to='/admin'>Administration</Link> : '' }&nbsp;&nbsp;
+                { this.state.isAuthenticated == false ? <a href="/login">Se connecter</a> : <a href="/logout">Connecté {this.state.user.login}</a> }
             </div>
         )
     }
---
+++ gateway/src/main/front/app/data/UserContext.js
@@ -0,0 +1,17 @@
+/**
+ * Created by t0135150 on 01/03/2017.
+ */
+export default class UserContext {
+
+    constructor(login, authorities) {
+        this.login = login;
+        this.authorities = authorities;
+    }
+
+    hasAuthority(authority) {
+        if (this.authorities != undefined) {
+            return Array.from(this.authorities).indexOf(authority) > -1;
+        }
+        return false
+    };
+}
