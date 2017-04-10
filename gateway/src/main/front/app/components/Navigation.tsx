cdcddf93dd9280b4392554843a9b569c76a831f7
Thomas Vautrin <thomas-externe.vautrin@edf.fr>
2017-04-10 17:23:38 +0200

    Mise en place BDD Postgresql

--- gateway/src/main/front/app/components/Navigation.tsx
+++ gateway/src/main/front/app/components/Navigation.tsx
@@ -36,8 +36,8 @@ export default class Navigation extends React.Component<NavigationProps, Navigat
         return (
             <div>
                 <Link to='/'>Home</Link>&nbsp;
-                { this.state.user.hasAuthority('ROLE_ADMIN') ? <Link to='/search'>Recherche</Link> : '' }&nbsp;&nbsp;
-                { this.state.user.hasAuthority('ROLE_ADMIN') ? <Link to='/admin'>Administration</Link> : '' }&nbsp;&nbsp;
+                { this.state.user.hasAuthority('ADMIN') ? <Link to='/search'>Recherche</Link> : '' }&nbsp;&nbsp;
+                { this.state.user.hasAuthority('ADMIN') ? <Link to='/admin'>Administration</Link> : '' }&nbsp;&nbsp;
                 { this.state.user.authenticated ?  <a href="/logout">({this.state.user.login})&nbsp;Déconnexion</a> :
                     <a href="/login">Connexion</a> }
             </div>
